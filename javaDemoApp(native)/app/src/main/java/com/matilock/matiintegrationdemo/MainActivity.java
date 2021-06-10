f010e48abc5a8d0505f31277081d705bc91e3f8c
andrey.posnov <andrey.posnov@mati.io>
2021-06-10 19:27:41 +0300

    add nati android demo app

---
+++ javaDemoApp(native)/app/src/main/java/com/matilock/matiintegrationdemo/MainActivity.java
@@ -0,0 +1,43 @@
+package com.matilock.matiintegrationdemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.getmati.mati_sdk.MatiButton;
+import com.getmati.mati_sdk.MatiSdk;
+import com.getmati.mati_sdk.Metadata;
+
+
+public class MainActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MatiButton btn = findViewById(R.id.matiKYCButton);
+
+        btn.setParams("YOUR_CLIENT_ID",
+                "YOUR_FLOW_ID",
+                new Metadata.Builder()
+                        .with("userId", "qwfguweo")
+                        .with("type", 2)
+                        .build());
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == MatiSdk.REQUEST_CODE && data!=null) {
+            if(resultCode == RESULT_OK) {
+                Toast.makeText( this,"SUCCESS | VerificationId: " + data.getStringExtra(MatiSdk.ARG_VERIFICATION_ID), Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText( this,"CANCELLED | VerificationId: " + data.getStringExtra(MatiSdk.ARG_VERIFICATION_ID), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}

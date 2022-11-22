1044e9a87962eef12b76f288c037a75eca54f330
Anurag Chauhan <anuragchauhan@Anurags-MacBook-Pro.local>
2022-11-23 02:21:27 +0530

    Update reactnative app- android stable

--- reactNativeDemoApp/App.js
+++ reactNativeDemoApp/App.js
@@ -1,117 +1,47 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {Component} from 'react';
+import {NativeModules, NativeEventEmitter, Button, View} from 'react-native';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  Button,
-} from 'react-native';
+import {MetaMapRNSdk} from 'react-native-metamap-sdk';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default class App extends Component {
+  constructor() {
+    super();
+    console.log('Constructor Called.');
+  }
 
-import {
-  MetaMapRNSdk,
-} from 'react-native-metaMap-sdk';
+  componentDidMount() {
+    //set listening callbacks
+    const MetaMapVerifyResult = new NativeEventEmitter(
+      NativeModules.MetaMapRNSdk,
+    );
+    MetaMapVerifyResult.addListener('verificationSuccess', data =>
+      console.log(data),
+    );
+    MetaMapVerifyResult.addListener('verificationCanceled', data =>
+      console.log(data),
+    );
+  }
 
+  //call showFlow when button is clicked
+  handleMetaMapClickButton = () => {
+    //set 3 params clientId (cant be null), flowId, metadata
+    var yourMetadata = {param1: 'value1', param2: 'value2'};
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    MetaMapRNSdk.showFlow('YOUR_CLIENT_ID', 'YOUR_FLOW_ID', yourMetadata);
   };
 
-  handleMetaMapClickButton = () => {
-  //set 3 params clientId (cant be null), flowId, metadata
-  	  var yourMetadata = { }
-
-   	 MetaMapRNSdk.showFlow("610b96fb7cc893001b135505", "61276bad2b7763001b80976f", yourMetadata);
-     console.log("hello world 123");
- }
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Button onPress={this.handleMetaMapClickButton} title="Click here"/>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+  //Add button to view graph
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'powderblue',
+        }}>
+        <Button onPress={this.handleMetaMapClickButton} title="Click here" />
+      </View>
+    );
+  }
+}

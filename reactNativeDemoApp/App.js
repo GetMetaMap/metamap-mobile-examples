e2db78f63c7c857e83c0bce29cdeab76b0a3ab79
Avo_Sukiasyan <avo.sukaisyan@gmail.com>
2023-04-17 19:43:15 +0400

    updated project

--- reactNativeDemoApp/App.js
+++ reactNativeDemoApp/App.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react';
-import {NativeModules, NativeEventEmitter, Button, View} from 'react-native';
+import {
+  NativeModules,
+  NativeEventEmitter,
+  Button,
+  View
+} from 'react-native';
 
-import {MetaMapRNSdk} from 'react-native-metamap-sdk';
+import {
+  MetaMapRNSdk,
+} from 'react-native-metamap-sdk';
 
 export default class App extends Component {
   constructor() {
@@ -10,25 +17,20 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    //set listening callbacks
-    const MetaMapVerifyResult = new NativeEventEmitter(
-      NativeModules.MetaMapRNSdk,
-    );
-    MetaMapVerifyResult.addListener('verificationSuccess', data =>
-      console.log(data),
-    );
-    MetaMapVerifyResult.addListener('verificationCanceled', data =>
-      console.log(data),
-    );
+	 //set listening callbacks
+  	const MetaMapVerifyResult = new NativeEventEmitter(NativeModules.MetaMapRNSdk)
+ 	 MetaMapVerifyResult.addListener('verificationSuccess', (data) => console.log(data))
+ 	 MetaMapVerifyResult.addListener('verificationCanceled', (data) => console.log(data))
   }
 
   //call showFlow when button is clicked
   handleMetaMapClickButton = () => {
-    //set 3 params clientId (cant be null), flowId, metadata
-    var yourMetadata = {param1: 'value1', param2: 'value2'};
 
-    MetaMapRNSdk.showFlow('YOUR_CLIENT_ID', 'YOUR_FLOW_ID', yourMetadata);
-  };
+	 //set 3 params clientId (cant be null), flowId, metadata
+  	  var yourMetadata = { param1: "value1", param2: "value2" }
+
+   	 MetaMapRNSdk.showFlow("YOUR_CLIENT_ID", "YOUR_FLOW_ID", yourMetadata);
+  }
 
   //Add button to view graph
   render() {
@@ -40,7 +42,7 @@ export default class App extends Component {
           alignItems: 'center',
           backgroundColor: 'powderblue',
         }}>
-        <Button onPress={this.handleMetaMapClickButton} title="Click here" />
+        <Button onPress={this.handleMetaMapClickButton} title="Click here"/>
       </View>
     );
   }
